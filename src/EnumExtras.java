63be1f9443e21bd3f5f638846c42d67da3c7a06e
Jason <JasonWHowse@gmail.com>
2021-03-09 15:34:44 -0600

    S03.26. Java Fundamentals: Out of the Ordinary Concepts Part 2

---
+++ src/EnumExtras.java
@@ -0,0 +1,57 @@
+/*
+The Learn Programming Academy
+Java SE 11 Developer 1Z0-819 OCP Course - Part 2
+Section 1: Java Fundamentals 
+Topic:  Enum Extras
+*/
+enum PrimaryColor {
+    RED(1) {
+        {
+            // initializer for the individual class constants
+            System.out.println(this + " initialized");
+        }
+    },//RED(1) {
+    BLUE(2) {
+
+        {
+            // initializer for the individual class constants
+            System.out.println(this + " initialized");
+        }
+    },//BLUE(2) {
+    YELLOW(3) {
+        {
+            // initializer for the individual class constants
+            System.out.println(this + " initialized");
+        }
+    };
+
+    int rating = 0;
+
+    // static initializer for the PrimaryColor class (enum)
+    static {
+        System.out.println("Enum Class Initialization");
+    }//static {
+
+    // initializer for all of the anonymous class constants
+    {
+        System.out.println("Enum Body Initialization for " + this);
+    }
+
+    // Constructor
+    PrimaryColor(int rating) {
+        System.out.println("Primary Color constructor for " + this);
+        this.rating = rating;
+    }//PrimaryColor(int rating) {
+}//enum PrimaryColor {
+
+public class EnumExtras {
+    public static void main(String[] args) {
+//        for (PrimaryColor color : PrimaryColor.values()) {
+//            System.out.println(color);
+//        }//for (PrimaryColor color : PrimaryColor.values()) {
+
+        System.out.println(PrimaryColor.RED);
+        System.out.println(PrimaryColor.BLUE);
+        System.out.println(PrimaryColor.YELLOW);
+    }//public static void main(String[] args) {
+}//public class EnumExtras {
