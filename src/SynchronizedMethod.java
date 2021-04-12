ece7249140c8abb86c94406e40fbda4e2ee8ab50
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-04-12 09:28:16 -0500

    S12.126. Race Conditions and Other Thread Issues

--- src/SynchronizedMethod.java
+++ src/SynchronizedMethod.java
@@ -5,13 +5,11 @@ Section 11: Concurrency
 Topic:  Shared data approaches
 */
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,26 +20,36 @@ class TheBallInTheAir {
     private String ballType = "Volleyball";
     // Total number of hits on the ball
 
-    private int hit;
+    private AtomicInteger hit = new AtomicInteger(0);
     // Map of players and their hits..
-    private Map<String, Integer> players = new TreeMap<>();
+    private Map<String, Integer> players = Collections.synchronizedMap(new TreeMap<>());
+    // Constructor
+    TheBallInTheAir() {
+        players.put("Jane", 0);
+        players.put("Mary", 0);
+        players.put("Ralph", 0);
+        players.put("Joe", 0);
+    }
 
     // Method called by threads, adds player to map if not found
     // or increments value in map
     public int addHit(String player) {
-        this.hit++;
+
         if (players.containsKey(player)) {
             players.computeIfPresent(player, (key, val) -> ++val);
         } else {//if (players.containsKey(player)) {
-            players.put(player, 1);
-
+            // A null is returned if no mapping was found
+            if (players.putIfAbsent(player, 1) != null) {
+                System.out.println("Encountered race condition");
+                players.computeIfPresent(player, (key, val) -> ++val);
+            }//if (players.putIfAbsent(player, 1) != null) {
         }//else {
-        return this.hit;
+        return hit.incrementAndGet();
     }//public int addHit(String player) {
 
     // Present writeable output
     public String toString() {
-        return "Total " + ballType + " hits: " + this.hit
+        return "Total " + ballType + " hits: " + this.hit.get()
                 + ", Player hits: " + players.values().stream().mapToInt(s -> s).sum()
                 + "\nPlayers List: " + players;
     }//public String toString() {
